f1d610f06ed221b8909785f2218df0f7cc7cedd2
Rahul kumar <rahul.kumar4@tothenew.com>
2022-04-07 18:54:03 +0530

    generate new password and login request

--- Routers/auth.js
+++ Routers/auth.js
@@ -1,22 +1,53 @@
 const router = require('express').Router();
+
 const User = require('../models/User')
+const bcrypt = require('bcrypt');
 
 router.post('/register', async (req, res) => {
-   const newUser=new User({
-       username:req.body.username,
-       email:req.body.email,
-       password:req.body.password
-   })
-   try{
-       const user= await newUser.save();
-       res.status(200).json(user);
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-   }catch(err)
-   {
-      console.log(err);
-   }
+
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save()
+    res.status(200).json(user);
+
+  }
+
+  catch (err) {
+    res.status(500).json(err)
+  }
 });
 
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    // console.log(user)
+    // console.log( req.body )
+
+    if (!user) {
+      return res.status(404).json("user not found");
+    }
+
+     const validPassword = await bcrypt.compare(req.body.password, user.password)
+
+    if (!validPassword) {
+      return res.status(400).json("wrong password")
+    }
+    // generateJwt token   
+
+    res.status(300).json(user);
+
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 
 module.exports = router;
